e8ef157a5625b3ab8a827691f664e0b403ff81d4
JOSHCLUNE <josh.seth.clune@gmail.com>
2026-03-31 14:33:37 -0400

    Fix the commit of Duper's dependency

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.22.0"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "querySMT"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "e4f158b4142d5ccf9f1327dd9b77c261ac9ae85f"
 
 require «premise-selection» from git "https://github.com/hanwenzhu/premise-selection" @ "v4.22.0"
 
